2c99e50283f7ede6ae2f2e51e9fc317fd80c49e5
vips120 <vipulsingh0000@gmail.com>
2019-08-07 18:43:12 +0530

    #inputandoutput

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {CourseComponent} from './courses.component'
 import {Courses} from './courses';
+import {TaskComponent} from './task.component';
+import { ParentComponent } from './parent.component';
+import { ChildComponent } from './child.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    TaskComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
